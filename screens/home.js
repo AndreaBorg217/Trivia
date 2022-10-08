e8fdfa208b82c81af3c848e7c8e8f330fe30a3cc
Andrea Borg <andreaborg217@gmail.com>
2022-10-08 10:02:16 +0200

    finished home screen

--- screens/home.js
+++ screens/home.js
@@ -5,6 +5,11 @@ import {View, StyleSheet, Image, TouchableOpacity, Text} from 'react-native';
  const Home = ({navigation}) => {
      return(
      <View style = {styles.container}>
+        <Image style={styles.name} source={require('../assets/images/name.png')}/>
+        <Image style={styles.logo} source={require('../assets/images/unlit_bulb.png')}/>
+        <TouchableOpacity style={styles.playButton} onPress={() => navigation.navigate("Play")}>
+            <Text style={styles.playText}>Play</Text>
+        </TouchableOpacity>
      </View>
    );
   };
@@ -18,6 +23,33 @@ import {View, StyleSheet, Image, TouchableOpacity, Text} from 'react-native';
      alignItems: 'center',
      justifyContent: 'center',
    },
+   name:{
+    width: 250,
+    height: 100,
+    position: 'absolute',
+    transform: [{translateY: -250}]
+   },
+   logo:{
+    width: 250,
+    height: 250,
+    marginLeft: 20,
+    position: 'absolute',
+   },
+   playButton:{
+    backgroundColor: '#2E2F5B',
+    width: 180,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 40,
+    position: 'absolute',
+    transform: [{translateY: 250}]
+   },
+   playText:{
+    color: '#FFFFFF',
+    fontFamily: 'BubblegumSans-Regular',
+    fontSize: 30,
+   }
   });
  
  export default Home;
